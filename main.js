651b72279166fbb8bf7250a3990b8c7b3a1069b0
Yeison-Soberanis <yeisonvi13@gmail.com>
2025-02-17 21:20:50 -0600

    Footer and Responsive

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
 //  Mune
-let menu = document.querySelector(".menu-icon");
+let menu = document.querySelector(".menu-icon"); 
+// document.querySelector(".menu-icon") selecciona el elemento con la clase menu-icon (botón de menú).
+let navbar = document.querySelector(".navbar"); 
 
+// "onclick" significa que la función se ejecutará cuando el usuario haga clic en el ícono del menú.
 menu.onclick = () => {
+    // Agrega la clase "open-menu" al navbar si no la tiene; si ya la tiene, la elimina.
+    // Esto permite mostrar u ocultar el menú de navegación dinámicamente.
     menu.classList.toggle("move");
+// animacion de abrir el menu 
+    navbar.classList.toggle("open-menu");
+};
+// Clase Menu On Page Scroll lo que hace es oculta el despliegue del menu cuando bajas usando el mouse
+window.onscroll = () => {
+    menu.classList.remove("move");
+    navbar.classList.remove("open-menu");
+};
+// Input Form Date
+window.onload = () => {
+    let today = new Date().toISOString().split("T")[0];
+    document.getElementById("start-date").value = today;
+    document.getElementById("return-date").value = new Date(
+        Date.now() + 7 * 86400000
+    )
+    .toISOString()
+    .split("T")[0];
 };
